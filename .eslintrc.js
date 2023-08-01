7d3b89856a9c94c37a8d48075934085107c83167
최시온 <sion@interpark.com>
2023-08-01 13:54:34 +0900

    storybook monorepo

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['get-off-my-lawn', 'plugin:storybook/recommended'],
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+    'node/no-extraneous-import': 0
+  }
+};
