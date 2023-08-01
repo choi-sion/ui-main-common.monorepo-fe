c33f98b351f1b2f3a982719caca9d7dbcabd5fe4
최시온 <sion@interpark.com>
2023-08-01 15:08:02 +0900

    폴더 구조 변경

--- packages/views/tour/componets/button/Button.jsx
+++ packages/views/tour/componets/button/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '/packages/tour/componets/button/button.scss';
+import '/packages/styles/tour/componets/button/button.scss';
 
 
 /**
